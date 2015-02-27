7c861b1f51b47cb0e405be9b72f76bac5b449203
starnight <starnight@g.ncu.edu.tw>
2015-02-27 12:39:41 +0000

    lab-2

--- lab-2/mutex.s
+++ lab-2/mutex.s
@@ -9,7 +9,14 @@
 	.type lock_mutex, function
 lock_mutex:
         @ INSERT CODE BELOW
-
+	push {r3, r4}
+	ldr r4, =locked
+.L1:
+	ldr r3, [r0]
+	cmp r3, #0
+	bne .L1
+	str r4, [r0]
+	pop {r3, r4}
         @ END CODE INSERT
 	bx lr
 
@@ -19,7 +26,10 @@ lock_mutex:
 	.type unlock_mutex, function
 unlock_mutex:
 	@ INSERT CODE BELOW
-        
+    push {r4}
+	ldr r4, =unlocked
+	str r4, [r0]
+	pop {r4}
         @ END CODE INSERT
 	bx lr
 	.size unlock_mutex, .-unlock_mutex
